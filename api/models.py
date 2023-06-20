85ffae0b21226f56591b54d739b3097c2adc4e6e
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-20 23:09:50 +0000

    Added a user model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class Society(models.Model):
@@ -45,3 +46,14 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    #email = models.CharField(max_length=255, unique=True)
+    email = models.EmailField('email adress', unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+

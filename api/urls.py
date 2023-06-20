8ec9cd0c12f3efc1392056ecf7795fa59fb2bad3
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-20 23:10:10 +0000

    Added authentication routes

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BudgetCreateView, CalculateBudget, GetSocietyBudgets
+from .views import BudgetCreateView, CalculateBudget, GetSocietyBudgets, RegisterView, LoginView, UserView, LogoutView
+
 
 urlpatterns = [
     path('create-budget', BudgetCreateView.as_view(), name='budget-create'),
     path('calculate-budget', CalculateBudget.as_view() ),
     path('get-budgets/<str:id>',GetSocietyBudgets.as_view() ),
+
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
 ]

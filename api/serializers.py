630d953e0209735f7ed48cf210d4b1bcf948154b
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-20 23:09:35 +0000

    Implemented authentication with jwt

--- api/serializers.py
+++ api/serializers.py
@@ -1,27 +1,7 @@
-""" from rest_framework import serializers
-from .models import Budget, Item, Society
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = ['name', 'unit_price', 'quantity']
-
-class BudgetSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True)
-
-    class Meta:
-        model = Budget
-        fields = ['title', 'items']
-
-
-class SocietySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Society
-        fields = "__all__"
-
- """
 from rest_framework import serializers
 from .models import Budget, Item, Society
+from .models import User
+
 
 
 class ItemSerializer(serializers.ModelSerializer):
@@ -55,3 +35,21 @@ class SocietySerializer(serializers.ModelSerializer):
     class Meta:
         model = Society
         fields = "__all__"
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance

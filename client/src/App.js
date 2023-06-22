b201198bc837f8fdf4d1c342c52d6bc139ef697d
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-22 22:41:10 +0000

    Added authentication in the fronted

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
-import HomePage from './components/HomePage';
+import "./App.css";
+import HomePage from "./components/HomePage";
+import { Route, Router, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    setName("Listowel")();
+  });
+
   return (
     <div className="App">
-      <HomePage />
+      <div className="App">
+        <Router>
+          <Nav name={name} setName={setName} />
+          <Routes>
+            <Route path="/" element={<HomePage name={name} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }

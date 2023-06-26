4f17e31aca55efdaf9350e13c9640dc62ba128fc
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-26 22:26:06 +0000

    Added login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "./Login.css"
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
 
+  const createBudget = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/api/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        toast.success("Logged in successfully!");
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   const submit = async (e) => {
     e.preventDefault();
-
     const response = await fetch("http://localhost:8000/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -20,16 +38,20 @@ const Login = (props) => {
     });
 
     const content = await response.json();
+    toast.success("Logged in successfully!");
+    console.log(content)
 
     setRedirect(true);
-    props.setName(content.name);
   };
 
   if (redirect) {
+    alert("User logged in successfully")
     return <Navigate to="/" />;
   }
 
   return (
+    <div><ToastContainer />
+    <br/>
     <form onSubmit={submit}>
       <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
       <input
@@ -39,6 +61,7 @@ const Login = (props) => {
         required
         onChange={(e) => setEmail(e.target.value)}
       />
+      <br/>
 
       <input
         type="password"
@@ -47,12 +70,13 @@ const Login = (props) => {
         required
         onChange={(e) => setPassword(e.target.value)}
       />
+      <br/>
 
       <button className="w-100 btn btn-lg btn-primary" type="submit">
-        Sign in
+        Login
       </button>
     </form>
-  );
+  </div>);
 };
 
 export default Login;

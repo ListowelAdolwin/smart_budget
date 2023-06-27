59fd01d95c04af67b50afc87b706bfc4f13d767b
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-27 22:52:24 +0000

    Added register and login routes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,43 @@
-import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
 
 const NavbarSection = () => {
+  const logout = async () => {
+    await fetch("http://localhost:8000/api/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    return <Navigate to="/" />;
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home" className="mr-auto ps-5">
+      <Navbar.Brand href="/" className="mr-auto ps-5">
         smartBudget
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto"></Nav>
         <Nav className="me-5">
-          <Nav.Link href="#account" className="me-4">
-            Account
-          </Nav.Link>
-          <Button variant="outline-light" className="">new budget</Button>
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <li className="nav-item active">
+              <Link to="/login">
+                <Button>Login</Button>
+              </Link>
+            </li>
+            <li className="nav-item ms-2 me-2">
+              <Link to="/register">
+                <Button>Register</Button>{" "}
+              </Link>
+            </li>
+            <li>
+              <Button onClick={logout}> Logout</Button>
+            </li>
+          </ul>
+          <Button variant="outline-light" className="mx-2">
+            new budget
+          </Button>
         </Nav>
       </Navbar.Collapse>
     </Navbar>

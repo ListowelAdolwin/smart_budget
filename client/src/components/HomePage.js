3c9ef050d180889ce4cdf90f0810cb8d12aac8d2
ListowelAdolwin <listoweladolwin@gmail.com>
2023-09-13 23:58:04 +0000

    Updated homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,19 +1,41 @@
-import React from "react";
-import NavbarSection from "./NavBar";
+import React, {useContext} from "react";
 import axios from "axios";
-import { useState } from "react";
+import {useNavigate} from "react-router-dom"
+import { useState, useEffect} from "react";
 import { Container, Table, Button, Form } from "react-bootstrap";
 import "tailwindcss/tailwind.css";
 import "./HomePage.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AuthContext from "./context/AuthContext";
 
 export default function HomePage(props) {
   const [itemName, setItemName] = useState("");
   const [quantity, setQuantity] = useState("");
   const [unitPrice, setUnitPrice] = useState("");
+  const [title, setTitle] = useState("");
   const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [nhil, setNhil] = useState(0);
+  const [vat, setVat] = useState(0);
+  const [grandTotal, setgrandTotal] = useState(0);
+  const [subTotal, setSubTotal] = useState(0);
   const [showForm, setShowForm] = useState(false);
+  const [showTax, setShowTax] = useState(false);
+
+  const navigate = useNavigate();
+  let {user} = useContext(AuthContext)
+
+  useEffect(() => {
+    const nhil = Math.round(Number(0.06 * total) * 100) / 100;
+    const subTotal = Math.round(Number(nhil + total) * 100) / 100;
+    const vat = Math.round(Number(subTotal * 0.15) * 100) / 100;
+    const grandTotal = Math.round(Number(vat + subTotal) * 100) / 100;
+    setNhil(nhil);
+    setVat(vat);
+    setgrandTotal(grandTotal);
+    setSubTotal(subTotal);
+  }, [quantity, unitPrice, total]);
 
   const handleAddItem = () => {
     const newItem = {
@@ -26,47 +48,59 @@ export default function HomePage(props) {
     setQuantity("");
     setUnitPrice("");
     setShowForm(false);
+    setTotal(total + quantity * unitPrice);
+    setShowTax(true);
   };
 
   const createBudget = () => {
     axios
       .post("http://127.0.0.1:8000/api/create-budget", {
-        title: "Algorithmic Quiz",
-        society_id: props.id,
+        title: title,
+        society_id: user.id,
         items: items,
       })
       .then((response) => {
         toast.success("Budget created successfully!");
-        console.log(response.data);
         setItems([]);
+        setTitle("");
+        setShowTax(false);
       })
       .catch((error) => {
         console.error(error);
       });
   };
 
-  const getBudget = () => {
-    //`/room/${roomCode}`
-    axios.get("http://127.0.0.1:8000/api/get-budgets/`{props.id}`").then((res) => {
-      console.log(res.data);
-    });
-  };
-
   return (
-    <div>
+    <div className="homepage">
       <ToastContainer />
-
       <Container className="p-4">
-        <h1 className="text-2xl font-bold mb-4" onClick={getBudget}>
-          Item List
+      <Button onClick={() => {navigate("/")}}>dashboard</Button>
+      <br/>
+      <br/>
+        <h1 className="text-2xl font-bold mb-4">
+          Budget items
         </h1>
-
+        <div className="budget-title">
+          <Form className="">
+            <Form.Group>
+              <Form.Control
+                required
+                type="text"
+                value={title}
+                placeholder="Enter budget title"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </div>
+        <h1 className="budget-title">{title}</h1>
         <Table striped bordered hover responsive>
           <thead>
             <tr>
               <th>Item Name</th>
               <th>Quantity</th>
               <th>Unit Price</th>
+              <th>Total</th>
             </tr>
           </thead>
           <tbody>
@@ -75,8 +109,55 @@ export default function HomePage(props) {
                 <td>{item.name}</td>
                 <td>{item.quantity}</td>
                 <td>{item.unit_price}</td>
+                <td>{item.quantity * item.unit_price}</td>
               </tr>
             ))}
+            {showTax && (
+              <>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <b>Total</b>
+                  </td>
+                  <td>
+                    <b>{total}</b>
+                  </td>
+                </tr>
+                <tr>
+                  <td>NHIL/GetFund(6%)</td>
+                  <td></td>
+                  <td></td>
+                  <td>{nhil}</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <b>Sub-Total</b>
+                  </td>
+                  <td>
+                    <b>{subTotal}</b>
+                  </td>
+                </tr>
+                <tr>
+                  <td>VAT (15%)</td>
+                  <td></td>
+                  <td></td>
+                  <td>{vat}</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <b>Grand Total</b>
+                  </td>
+                  <td>
+                    <b>{grandTotal}</b>
+                  </td>
+                </tr>
+              </>
+            )}
           </tbody>
         </Table>
 
@@ -89,10 +170,11 @@ export default function HomePage(props) {
         </Button>
 
         {showForm && (
-          <Form className="bg-gray-200 p-4 rounded mb-4">
+          <Form className="bg-gray-200 rounded mb-4">
             <Form.Group className="mb-2" controlId="itemName">
               <Form.Label>Item Name</Form.Label>
               <Form.Control
+                required
                 type="text"
                 value={itemName}
                 onChange={(e) => setItemName(e.target.value)}
@@ -102,6 +184,7 @@ export default function HomePage(props) {
             <Form.Group className="mb-2" controlId="quantity">
               <Form.Label>Quantity</Form.Label>
               <Form.Control
+                required
                 type="text"
                 value={quantity}
                 onChange={(e) => setQuantity(e.target.value)}
@@ -111,6 +194,7 @@ export default function HomePage(props) {
             <Form.Group className="mb-2" controlId="unitPrice">
               <Form.Label>Unit Price</Form.Label>
               <Form.Control
+                required
                 type="text"
                 value={unitPrice}
                 onChange={(e) => setUnitPrice(e.target.value)}

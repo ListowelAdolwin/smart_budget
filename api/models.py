e088f3b856e72228e00ad3da4921d1ef366b737c
ListowelAdolwin <listoweladolwin@gmail.com>
2023-09-24 22:19:35 +0000

    Updated the society model

--- api/models.py
+++ api/models.py
@@ -2,11 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-""" class Society(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name """
 class Society(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.EmailField('email adress', unique=True)
